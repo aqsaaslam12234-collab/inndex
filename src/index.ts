2f84b7441220db119d6fdc7702d5da8b21e2cee6
AqsaAslam786 <aqsaaslam1724@gmail.com>
2025-11-30 07:03:20 +0000

    save

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// const ar1 = [1,2,3,4,5];
+// const ar2 = [1,2,3,4,5];
+// function isEqual(arr1: string | any[], arr2: string | any[]): boolean {
+//     if (arr1.length !== arr2.length){
+//         return true;
+//     }
+// }
+
+const ar1 = [1,2,3,4,5,14,3,2 ];
+const arSet =new Set(ar1)
+console.log(arSet)
+// for (let i = 0; i < ar1.length; i++) {
+//     if (count (ar1, ar1[i]) > 1) {
+//         console.log(ar1[i]);
+//     }
+// }
